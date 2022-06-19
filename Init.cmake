0da07239db6769f3604966346f1a90223311a2d3
ResetOTER <vgvgvvvdc@gmail.com>
2022-06-19 18:30:22 +0800

    cpm auto update

--- Init.cmake
+++ Init.cmake
@@ -95,6 +95,7 @@ endmacro()
 
 # invoke before project
 macro(ReMake_UseConan)
+	message(STATUS "Use Conan at ${REMAKE_ROOT}/conan.cmake")
 	set(USE_CONAN true)
 	include(${REMAKE_ROOT}/conan.cmake)
 endmacro()
@@ -103,5 +104,23 @@ endmacro()
 macro(ReMake_UseCPM)
 	set(USE_CPM true)
 	add_definitions(-DCPM_SOURCE_CACHE=${REMAKE_TEMP_ROOT}/CMP/deps)
-	include(${REMAKE_ROOT}/CPM/CPM.cmake)
+
+	set(CPM_DOWNLOAD_VERSION 0.35.1)
+	if(CPM_SOURCE_CACHE)
+		set(CPM_DOWNLOAD_LOCATION "${CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
+	elseif(DEFINED ENV{CPM_SOURCE_CACHE})
+		set(CPM_DOWNLOAD_LOCATION "$ENV{CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
+	else()
+		set(CPM_DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
+	endif()
+
+	if(NOT (EXISTS ${CPM_DOWNLOAD_LOCATION}))
+		message(STATUS "Downloading CPM.cmake to ${CPM_DOWNLOAD_LOCATION}")
+		file(DOWNLOAD
+				https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
+				${CPM_DOWNLOAD_LOCATION}
+				)
+	endif()
+
+	include(${CPM_DOWNLOAD_LOCATION})
 endmacro()
